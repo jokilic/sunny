205f81aac9bf9387d6f69bf21fe0a011312fe0a4
Josip Kilić <neksuses@gmail.com>
2023-03-04 16:45:41 +0100

    Update UserAgent

--- lib/services/network.dart
+++ lib/services/network.dart
@@ -1,6 +1,3 @@
-import 'dart:convert';
-import 'dart:io';
-
 import 'package:dio/dio.dart';
 
 class Network {
@@ -16,9 +13,7 @@ class Network {
         headers: {
           'Content-Type': 'application/json',
           'Accept-Charset': 'utf-8',
-          'User-Agent': Platform.isIOS
-              ? 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.3 Mobile/15E148 Safari/604.1'
-              : 'Mozilla/5.0 (Linux; Android 13) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.5481.153 Mobile Safari/537.36',
+          'User-Agent': 'josipkilic.com neksuses@gmail.com',
         },
       ),
     );
