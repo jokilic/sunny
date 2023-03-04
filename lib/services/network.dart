5117f025e4512455be34512002b3bd0e6ee72f9b
Josip Kilić <neksuses@gmail.com>
2023-03-04 16:43:37 +0100

    Fix API issues

--- lib/services/network.dart
+++ lib/services/network.dart
@@ -1,18 +1,30 @@
-import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'dart:io';
+
+import 'package:dio/dio.dart';
 
 class Network {
   final String url;
+  final Dio dio;
 
-  Network(this.url);
+  Network(this.url) : dio = Dio();
 
   Future getData() async {
-    http.Response response = await http.get(Uri.parse(url));
+    final response = await dio.get(
+      url,
+      options: Options(
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept-Charset': 'utf-8',
+          'User-Agent': Platform.isIOS
+              ? 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.3 Mobile/15E148 Safari/604.1'
+              : 'Mozilla/5.0 (Linux; Android 13) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.5481.153 Mobile Safari/537.36',
+        },
+      ),
+    );
 
     if (response.statusCode == 200) {
-      String data = response.body;
-
-      return jsonDecode(data);
+      return response.data;
     } else {
       print('Sunny: Network Error - ${response.statusCode}');
     }
